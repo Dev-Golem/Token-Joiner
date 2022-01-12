8c962275a96476b5664033e571ac843c18986aa7
Dev-Golem <82716520+Dev-Golem@users.noreply.github.com>
2022-01-12 16:51:59 +0000

    Add files via upload

---
+++ tokenjoiner.py
@@ -0,0 +1,84 @@
+import requests, discord, http.client, json
+from colorama import Fore
+
+http.client._is_legal_header_name = re.compile(rb'[^\s][^:\r\n]*').fullmatch
+
+tokens = open("tokens.txt", "r").read().splitlines()
+
+def join(invite, token):
+            token = token.replace("\r", "")
+            token = token.replace("\n", "")
+            headers = {
+                ":authority": "discord.com",
+                ":method": "POST",
+                ":path": "/api/v9/invites/" + invite,
+                ":scheme": "https",
+                "accept": "*/*",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US",
+                "Authorization": token,
+                "content-length": "0",
+                "cookie": f"__cfuid={randstr(43)}; __dcfduid={randstr(32)}; locale=en-US",
+                "origin": "https://discord.com",
+                'referer': 'https://discord.com/channels/@me',
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.600 Chrome/91.0.4472.106 Electron/13.1.4 Safari/537.36",
+                "x-context-properties": "eyJsb2NhdGlvbiI6Ikludml0ZSBCdXR0b24gRW1iZWQiLCJsb2NhdGlvbl9ndWlsZF9pZCI6Ijg3OTc4MjM4MDAxMTk0NjAyNCIsImxvY2F0aW9uX2NoYW5uZWxfaWQiOiI4ODExMDg4MDc5NjE0MTk3OTYiLCJsb2NhdGlvbl9jaGFubmVsX3R5cGUiOjAsImxvY2F0aW9uX21lc3NhZ2VfaWQiOiI4ODExOTkzOTI5MTExNTkzNTcifQ==",
+                "x-debug-options": "bugReporterEnabled",
+                "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJjYW5hcnkiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC42MDAiLCJvc192ZXJzaW9uIjoiMTAuMC4yMjAwMCIsIm9zX2FyY2giOiJ4NjQiLCJzeXN0ZW1fbG9jYWxlIjoic2siLCJjbGllbnRfYnVpbGRfbnVtYmVyIjo5NTM1MywiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0="
+            }
+            rrr = requests.post("https://discord.com/api/v9/invites/" + invite, headers=headers)
+            if rrr.status_code == 204 or 200:
+                print(f'{Fore.LIGHTGREEN_EX}[>] {Fore.RESET} Done')
+            else:
+                print('[>] Error...')
+
+invite = input(f"\n[>] Discord Server Invite: ")
+invite = invite.replace("https://discord.gg/", "")
+invite = invite.replace("discord.gg/", "")
+invite = invite.replace("https://discord.com/invite/", "")
+
+delay = float(input(f'[>] Delay: '))
+
+for token in tokens:
+            time.sleep(delay)
+            threading.Thread(target=join, args=(invite, token)).start()
+
+time.sleep(3)
+
+b = input('[>] Do you want to bypass member screening y/n?: ')
+
+if b == 'y':
+            def bpss(invite_code, serverId, token):
+                headers = {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept-Language': 'en-US',
+                    'Cookie': f"__cfuid={randstr(43)}; __dcfduid={randstr(32)}; locale=en-US",
+                    'DNT': '1',
+                    'origin': 'https://discord.com',
+                    'TE': 'Trailers',
+                    'X-Super-Properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9',
+                    'authorization': token,
+                    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+                bpsur = f"https://discord.com/api/v9/guilds/{serverId}/member-verification?with_guild=false&invite_code=" + invite_code
+                r1 = requests.get(bpsur, headers=headers).json()
+                data = {}
+                data['version'] = r1['version']
+                data['form_fields'] = r1['form_fields']
+                data['form_fields'][0]['response'] = True
+                req = f"https://discord.com/api/v9/guilds/{str(serverId)}/requests/@me"
+                requests.put(req, headers=headers, json=data)
+
+            serverId = input('Server ID: ')
+            tokens = open('tokens.txt', 'r').read().splitlines()
+            for token in tokens:
+                threading.Thread(target=bpss, args=(invite, serverId, token)).start()
+
+elif b == 'n':
+    pass
+
+print(f'{Fore.LIGHTGREEN_EX}[>] {Fore.RESET} Done')
